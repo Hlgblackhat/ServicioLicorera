7d90c33058a3248f311d8273b8ba4300fe6fd44b
Haider López G <haiderlg@outlook.com>
2022-04-12 16:55:20 -0500

    Archivo de parametros de conexion con la base de datos.

---
+++ parametrosDB.php
@@ -0,0 +1,10 @@
+<?php
+//parametros de conexion
+  $servername = "localhost";
+  $usernameDB = "haider";
+  $passwordDB = "admin";
+  $nameDB = "serviciolc";
+  //iniciando conexion
+  $conexion = new mysqli($servername, $usernameDB, $passwordDB, $nameDB);
+
+ ?>
