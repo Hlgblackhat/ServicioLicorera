c1f3c40494744c454ae8539b1b520dfec4db86ea
Haider López G <haiderlg@outlook.com>
2022-04-14 02:13:14 -0500

    Agregando formulario de registrar nuevo producto y su respectivo controlador
    agregando archivo productos que muestra lista de todos los productos

---
+++ productos.php
@@ -0,0 +1,62 @@
+<?php
+include("parametrosDB.php");
+
+if($conexion->connect_error){
+  die("Conexion Fallida: ".$conexion->connect_error);
+}
+
+$consulta="SELECT * FROM productos";
+$resultado = $conexion->query($consulta);
+
+echo "<html>\n";
+echo "\t<head>\n";
+echo "\t\t<title>Lista de productos</title>\n";
+echo "\t\t<meta http-equiv= \"refresh\" content=\"5\" />\n";
+echo "\t\t<meta charset=\"UTF-8\"/>\n";
+echo "\t</head>\n";
+echo "\t<body>\n";
+
+if ($resultado->num_rows > 0) {
+    echo "<div align=\"center\">\n";
+    echo "<table border=2>\n";
+    echo "<tr BGCOLOR=\"#D3D3D3\">\n";
+    echo "<td>id_product</td>\n";
+    echo "<td>nombre</td>\n";
+    echo "<td>descripcion</td>\n";
+    echo "<td>precio</td>\n";
+    echo "<td colspan=\"2\">Acción</td>\n";
+    echo "</tr>";
+
+    $fila=1;
+    while($row = $resultado->fetch_assoc()) {
+        if($fila % 2 == 0){
+	        echo "<tr BGCOLOR=\"#D3D3D3\">\n";
+        }
+        else{
+          echo "<tr>\n";
+      	}
+        echo "<td>".$row["id_product"]."</td>\n<td>".$row["nombre"]."</td>\n<td>".$row["descripcion"]."</td>\n<td>".$row["precio"]."</
+        td>\n<td><a href=\"delete_producto.php?id=".$row["id_product"]."\">Delete</td><td><a href=\"update_producto.php?id=".$row["id_product"]."\">Update</td>\n";
+        echo "</tr>\n";
+        $fila=$fila+1;
+    }
+    echo "</table>\n";
+echo "</div>\n";
+
+echo "</body>\n";
+echo "</html>\n";
+
+} else {
+    echo "0 results";
+}
+
+$conexion->close();
+ ?>
+ <center>
+ <br><br>
+ <a href="new_producto.html">Registrar otro producto</a>
+ <br><br>
+ <a href="home_admin.php">Volver</a>
+ <br><br>
+ <a href="salir.php">Cerrar Session</a>
+ </center>
---
+++ valida_new_product.php
@@ -0,0 +1,36 @@
+<html>
+<body>
+
+<?php
+$id_product = $_POST["id_product"];
+$nombre = $_POST["nombre"];
+$desc = $_POST["descripcion"];
+$precio = $_POST["precio"];
+
+
+// Create connection
+include("parametrosDB.php");
+
+$consulta="INSERT INTO productos values('$id_product','$nombre','$desc','$precio')";
+
+if($conexion->query($consulta)===TRUE){
+?>
+<center><h1><?php echo 'Producto almacenado satisfactoriamente';
+}
+else{
+  ?>
+  <center><h1><?php echo 'Error al registrar el producto '.$conexion->error;
+}
+$conexion->close();
+?>
+</h1><br>
+<br>
+<br>
+<a href="home_admin.php">volver</a>
+<br>
+<br>
+<a href="new_producto.html">Registrar otro producto</a>
+</center>
+
+</body>
+</html>
