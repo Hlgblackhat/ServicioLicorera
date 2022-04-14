8fb5de5dbb2dc46d9f14badc7f7c27703e19ec12
Haider López G <haiderlg@outlook.com>
2022-04-14 02:46:14 -0500

    vista principal de adminitrador

---
+++ home_admin.php
@@ -0,0 +1,17 @@
+<html lang="es" dir="ltr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Página de Inicio</title>
+    </head>
+    <body>
+
+        <center>
+        <a href="new_producto.html">Registar Producto</a>
+        <br><br>
+        <a href="productos.php">Listar Productos</a>
+        <br><br>
+        <a href="salir.php">Cerrar Session</a>
+        </center>
+
+    </body>
+</html>
